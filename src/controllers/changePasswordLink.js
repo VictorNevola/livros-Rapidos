e2a47a515f68097a44feb4ea29e0640a73d4cc62
Victor Nevola <victornevolapn@gmail.com>
2019-11-20 22:01:26 -0300

    Create logic that if token expire, screen alter password dont render

--- src/controllers/changePasswordLink.js
+++ src/controllers/changePasswordLink.js
@@ -1,5 +1,19 @@
+const {UserChangeModel} = require("../models/userChange");
+
 const changePasswordLink = (request, response) => {
-    response.render('password');
+    UserChangeModel.findOne({'auth': request.query.token})
+    .then((user)=>{
+        if(user){
+            response.render('password');
+        }else {
+            response.render(`changePassword`, {
+                Message: `Link Expirado, enviar novamente a solicitação!`,
+            });
+        }
+    })
+    .catch((err)=>{
+        console.log(`Erro ChangePasswordLink`, err);
+    })
 }
 
 module.exports = changePasswordLink;
